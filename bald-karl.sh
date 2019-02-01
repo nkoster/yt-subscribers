659a12c6daf1fe8fa7f5a2740d7123c2d4448cd0
Niels Koster <n.koster@portavita.eu>
2019-02-01 23:22:21 +0100

    Less magic strings, better output.

--- bald-karl.sh
+++ bald-karl.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-curl -sLO https://w3b.net/database.yt
+db="database.yt"
 
-start_time=$(head -1 database.yt | awk -F '"' '{print$4}')
+echo "-- downloading $db"
+curl -sLO https://w3b.net/$db
+
+start_time=$(head -1 $db | awk -F '"' '{print$4}')
 now=$(date +"%s")
 total=$(echo $(($now-$start_time)))
+png="bald-karl.png"
 
-echo "---- $total"
+echo "-- $db contains $(($total/3600)) hours data."
 
 rrdtool create bald.rrd --step=1 --start=1548879240 \
     DS:ds1:GAUGE:5m:U:U RRA:AVERAGE:0:1:${total}s
 rrdtool create karl.rrd --step=1 --start=1548879240 \
     DS:ds1:GAUGE:5m:U:U RRA:AVERAGE:0:1:${total}s
 
-cat database.yt | grep bald | \
+cat $db | grep bald | \
     awk -v "rrdfile=bald.rrd" -F '"' \
     '{printf("rrdtool update %s %s:%s\n",rrdfile,$4,$8)}' >/tmp/rrdbald
-cat database.yt | grep Karl | \
+cat $db | grep Karl | \
     awk -v "rrdfile=karl.rrd" -F '"' \
     '{printf("rrdtool update %s %s:%s\n",rrdfile,$4,$8)}' >/tmp/rrdkarl
 
 sh /tmp/rrdbald
 sh /tmp/rrdkarl
 
-rrdtool graph bald-karl.png --start 1548879240 --end now \
+1>/dev/null rrdtool graph bald-karl.png --start 1548879240 --end now \
     -w 600 -h 300 \
     DEF:ds1a=bald.rrd:ds1:AVERAGE \
     DEF:ds2a=karl.rrd:ds1:AVERAGE \
     LINE2:ds1a#00a000:"Mr Bald" \
     LINE2:ds2a#0000a0:"Karl Rock"
+
+echo "-- $png has been created."
